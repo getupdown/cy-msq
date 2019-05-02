1f043e750400b70d70bc7b94c55402930acf3ef9
getupdown <443944620@qq.com>
2019-05-02 15:32:22 +0800

    编译脚本fix

--- broker_install_and_run.sh
+++ broker_install_and_run.sh
@@ -2,8 +2,13 @@
 
 set -e
 git pull
+# 先把core发布到本地仓库
+cd cy-msq-core
+mvn clean -U install  -DskipTests
+cd ..
+# 再package broker的部分
 cd cy-msq-broker
-mvn clean -U install -Pdev -DskipTests
+mvn clean -U package -Pdev -DskipTests
 mv target/msq-broker.jar /home/msq/project
 
 cd /home/msq/project
